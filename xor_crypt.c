7cccbab29b307cc74a30fa212b8cc5cef83ea8b1
alantsd <alantsd@hotmail.com>
2020-01-08 11:09:51 +0800

    Update xor_crypt.c

--- xor_crypt.c
+++ xor_crypt.c
@@ -1,9 +1,15 @@
 #include "xor_crypt.h"
 
-void xor_crypt(uint8_t* buf, size_t len, uint8_t* key)
+void xor_encrypt(uint8_t* buf, size_t len, uint8_t* key)
 {
   for(int i = 0; i < len; ++i)
   {
     buf[i] = buf[i] ^ key[i];
   }
 }
+
+void xor_decrypt(uint8_t* buf, size_t len, uint8_t* key)
+{
+    xor_encrypt(buf, len, key);
+}
+  
